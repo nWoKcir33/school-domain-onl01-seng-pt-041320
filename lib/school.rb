5b656efc17b5f770b4861cda3ef8a039d370888b
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-17 05:13:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,13 @@
 class School
-  def initialization(roster)
+  def initialize(roster)
+    @roster = roster
+  end
+  
+  def roster=(roster)
     @roster = roster
   end
   
   def roster
+    @roster
   end
 end
